c0056ac59247a6d3d5669ddbebc3ef1017dfbab7
Aina Gatell Hijazo <aina.gatell@gmail.com>
2023-01-25 16:21:37 +0100

    contact us css finished

--- resources/js/contact.js
+++ resources/js/contact.js
@@ -1,2 +1,6 @@
 /* Crea tu propia lógica para hacer un fetch que emule una post request a un servidor y enseñe un mensaje en consola cuando la llamada se resuelva */
-/*  ADVANCED: utiliza DOM manipulation para enseñarle al user que su mensaje se ha enviado correctamente o no */
+
+
+
+/* La página “Contáctanos” debe incluir un formulario para que los visitantes del sitio web puedan enviar una solicitud de contacto. El formulario debe incluir campos para recolectar información como el nombre, el correo electrónico y el mensaje del usuario. También se debe incluir un botón de envío para enviar la solicitud al punto final especificado en la URL. “https://jsonplaceholder.typicode.com/”.
+Los resultados de este formulario se deben ver registrados en consola una vez enviados al punto final especificado en la URL con un codigo de estado 201  */
