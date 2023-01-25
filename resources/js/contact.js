25bbf6bb5ebad6f2a460c46677e52a3b21e4994b
Aina Gatell Hijazo <aina.gatell@gmail.com>
2023-01-25 20:09:18 +0100

    patata

--- resources/js/contact.js
+++ resources/js/contact.js
@@ -1,2 +1,13 @@
 /* Crea tu propia lógica para hacer un fetch que emule una post request a un servidor y enseñe un mensaje en consola cuando la llamada se resuelva */
+
+
+const hamburger = document.querySelector(".hamburger")
+const navMenu = document.querySelector("#navlist")
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navlist.classList.toggle("active");
+
+})
+
 /*  ADVANCED: utiliza DOM manipulation para enseñarle al user que su mensaje se ha enviado correctamente o no */
