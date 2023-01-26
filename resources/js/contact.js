c9ebebc7f86b59d09687f6949e52848ba942c42f
Aina Gatell Hijazo <aina.gatell@gmail.com>
2023-01-26 10:16:13 +0100

    cambios hamburger

--- resources/js/contact.js
+++ resources/js/contact.js
@@ -2,12 +2,22 @@
 
 
 const hamburger = document.querySelector(".hamburger")
-const navMenu = document.querySelector("#navlist")
+const navMenu = document.querySelector(".nav-menu")
 
 hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("active");
-    navlist.classList.toggle("active");
+    navMenu.classList.toggle("active");
 
 })
 
+
+/* // Get the hamburger icon and navigation menu elements
+var hamburger = document.querySelector('.hamburger');
+var nav = document.querySelector('.nav');
+
+// Add an event listener to the hamburger icon to toggle the navigation menu when clicked
+hamburger.addEventListener('click', function() {
+  nav.classList.toggle('visible');
+});
+ */
 /*  ADVANCED: utiliza DOM manipulation para enseñarle al user que su mensaje se ha enviado correctamente o no */
